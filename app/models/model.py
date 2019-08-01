4d9ae9a24947d9a0f98dc57204d4a917e60d2d8b
EC2 Default User <ec2-user@ip-172-31-44-157.us-east-2.compute.internal>
2019-08-01 14:30:03 +0000

    mvp

---
+++ app/models/model.py
@@ -0,0 +1,11 @@
+def my_destination(city):
+    print("function running")
+    if city == "Madrid":
+        return("Museo Nacional del Prado, Puerta del Sol, Plaza Mayor, Plaza de Toros de Las Ventas")
+    elif city == "New York City":
+        return("Statue of Liberty, Hudson Yards, Times Square, Empire State Building, SoHo, World Trade Center, Central Park, Rockefeller Center")
+    elif city == "Chicago":
+        return("Millennium Park, Navy Pier, Cloud Gate, Willis Tower")
+    elif city == "Hong Kong":
+        return("Hong Kong Disneyland, Ocean Park (teens), Tian Tan Buddha, Avenue of Stars, Sky 100 Hong Kong Obswervation Deck")
+    
--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,15 @@ from app.models import model, formopener
 @app.route('/index')
 def index():
     return render_template("index.html")
+   
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    if request.method == 'GET':
+        return render_template("results.html")
+    else:
+        print(request.form)
+        userdata = dict(request.form)
+        print(userdata)
+        city = userdata["city"]
+        toDo = model.my_destination(city)
+        return render_template("results.html", a= city, b = toDo)
